e5a8b358d0e192020e6c953477910e54b2647123
Jaakko Pallari <jkpl@lepovirta.org>
2022-02-09 21:48:10 +0200

    Eliminate jitter in tests

--- internal/stat/stat_test.go
+++ internal/stat/stat_test.go
@@ -25,27 +25,27 @@ func TestUpdated(t *testing.T) {
 	u1, e1 := c1.Updated(c2)
 	u2, e2 := c2.Updated(c1)
 
-	assert.Equal(t, []string{
+	assert.ElementsMatch(t, []string{
 		"2.txt",
 		"4.txt",
 	}, u1)
-	assert.Equal(t, []string{
+	assert.ElementsMatch(t, []string{
 		"1.txt",
 	}, e1)
-	assert.Equal(t, []string{
+	assert.ElementsMatch(t, []string{
 		"2.txt",
 		"3.txt",
 	}, u2)
-	assert.Equal(t, []string{
+	assert.ElementsMatch(t, []string{
 		"1.txt",
 	}, e2)
 }
 
 func TestRemoved(t *testing.T) {
-	assert.Equal(t, []string{
+	assert.ElementsMatch(t, []string{
 		"3.txt",
 	}, c1.Removed(c2))
-	assert.Equal(t, []string{
+	assert.ElementsMatch(t, []string{
 		"4.txt",
 	}, c2.Removed(c1))
 }
